87723fedd3cb8266510c418412ce3c0a68092ddc
Jason L Perry <jason@suncoast.io>
2019-08-22 10:15:32 -0400

    add test coverage

--- day-04/bg-lib/test/test_helper.rb
+++ day-04/bg-lib/test/test_helper.rb
@@ -1,6 +1,11 @@
+require "simplecov"
+SimpleCov.start "rails"
+
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+
+Rails.application.eager_load!
 
 class ActiveSupport::TestCase
   include FactoryBot::Syntax::Methods
